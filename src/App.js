59ce27001be8d08ee40ab9b43a54de5b1b0c003e
nakowafers <nicola.kahale@gmail.com>
2021-04-16 22:39:40 -0400

    Adding services place holder and experimenting with flex
    Also broke starts into their own component, will need to still automate star creation, too much overhead with border box array.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { gsap } from 'gsap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Stars from './components/Stars';
 
 function App() {
   const slogan_1 = React.createRef();
@@ -25,15 +26,26 @@ function App() {
         <h1 ref={slogan_1} className="slogan">Dream.</h1>
         <h1 ref={slogan_2} className="slogan">Innovate.</h1>
         <h1 ref={slogan_3} className="slogan">Create.</h1>
+        <Stars />
       </section >
+      <section className="services">
+        <div>
+          <h2>Logo Design</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero qui reiciendis beatae itaque sunt hic non, officia quidem tempore laborum ipsum, aperiam rem. Itaque quam ducimus, soluta maiores voluptatem facilis.
+          Deserunt fugiat quam, accusamus iusto saepe cumque laborum dolore eos aperiam ducimus dolores aspernatur doloremque est maxime dolorum modi. Reprehenderit eaque laboriosam nostrum pariatur quia dolore nam minus illum inventore!</p>
+        </div>
+        <div>
+          <h2>Website Design</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero qui reiciendis beatae itaque sunt hic non, officia quidem tempore laborum ipsum, aperiam rem. Itaque quam ducimus, soluta maiores voluptatem facilis.
+          Deserunt fugiat quam, accusamus iusto saepe cumque laborum dolore eos aperiam ducimus dolores aspernatur doloremque est maxime dolorum modi. Reprehenderit eaque laboriosam nostrum pariatur quia dolore nam minus illum inventore!</p>
+        </div>
+      </section>
       <section className="head-shot">
         <img src="images/head_Shot.jpeg" alt="head_shot" />
         <h1>too busy working on your stuff<span>.</span></h1>
         <h2>more coming <span>soon...</span></h2>
       </section>
       <Footer />
-      <div className="ball-1"></div>
-      <div className="ball-2"></div>
     </div >
   );
 }
---
+++ src/components/Stars.js
@@ -0,0 +1,12 @@
+import './Stars.css';
+
+function Stars() {
+    return (
+        <div className="star-container">
+            <div className="ball-1"></div>
+            <div className="ball-2"></div>
+        </div>
+    );
+}
+
+export default Stars;
